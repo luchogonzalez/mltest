27cc12d39a7dffd237da094af031acd9836fbb30
Luciano Gonzalez <lucho.gonzalez125@gmail.com>
2017-02-11 17:45:59 -0300

    Added css tabs and tooltips

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 var zoom1 = new ch.Zoom(ch('#zoom-default')[0]);
 
-
 var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
     pagination: false
 });
 
 var dropdown1 = new ch.Dropdown(document.querySelector('.demo-dropdown1'));
-var dropdown2 = new ch.Dropdown(document.querySelector('.demo-dropdown2'), {
-	'shortcuts': false,
-});
+var dropdown2 = new ch.Dropdown(document.querySelector('.demo-dropdown2'));
 
-var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
+var tooltip1 = new ch.Tooltip(ch('.tooltip')[0]);
+var tooltip2 = new ch.Tooltip(ch('.tooltip')[1]);
+var tooltip3 = new ch.Tooltip(ch('.tooltip')[2]);
+var tooltip4 = new ch.Tooltip(ch('.tooltip')[3]);
+var tooltip5 = new ch.Tooltip(ch('.tooltip')[4]);
+var tooltip6 = new ch.Tooltip(ch('.tooltip')[5]);
